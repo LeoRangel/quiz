93d92459ee927a30d10876a8775228145387b601
LeoMdrs <leandro.mdrs06@gmail.com>
2021-09-26 19:08:59 -0300

    Adicionando animação ao clicar nas respostas

--- components/Resposta.tsx
+++ components/Resposta.tsx
@@ -12,43 +12,41 @@ interface RespostaProps {
 
 export default function Resposta(props: RespostaProps) {
     const resposta = props.valor
+    const respostaRevelada = resposta.revelada ? styles.respostaRevelada : ''
 
     return (
         <div
             className={styles.resposta}
             onClick={() => props.onResponse(props.indice)}
         >
-            <div className={styles.conteudoResposta}>
+            <div className={`${respostaRevelada} ${styles.conteudoResposta}`}>
 
-                {!resposta.revelada ? (
-                    <div className={styles.frente}>
-                        <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra }}>
-                            {props.letra}
-                        </div>
-                        <div className={styles.valor}>
-                            {resposta.valor}
-                        </div>
+                <div className={styles.frente}>
+                    <div className={styles.letra} style={{ backgroundColor: props.corFundoLetra }}>
+                        {props.letra}
                     </div>
-                ) : (
-                    <div className={styles.verso}>
-                        {resposta.certa ? (
-                            <div className={styles.certa}>
-                                <div className="">A resposta certa é...</div>
-                                <div className={styles.valor}>
-                                    {resposta.valor}
-                                </div>
-                            </div>
-                        ) : (
-                            <div className={styles.errada}>
-                                <div className="">A resposta certa é...</div>
-                                <div className={styles.valor}>
-                                    {resposta.valor}
-                                </div>
-                            </div>
-                        )}
+                    <div className={styles.valor}>
+                        {resposta.valor}
                     </div>
-                )}
+                </div>
 
+                <div className={styles.verso}>
+                    {resposta.certa ? (
+                        <div className={styles.certa}>
+                            <div className="">A resposta certa é...</div>
+                            <div className={styles.valor}>
+                                {resposta.valor}
+                            </div>
+                        </div>
+                    ) : (
+                        <div className={styles.errada}>
+                            <div className="">A resposta certa é...</div>
+                            <div className={styles.valor}>
+                                {resposta.valor}
+                            </div>
+                        </div>
+                    )}
+                </div>
 
             </div>
         </div>

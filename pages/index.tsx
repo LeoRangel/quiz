13a762c4d4823291bad1f8f631d4fc42ea39bff5
LeoMdrs <leandro.mdrs06@gmail.com>
2021-09-26 19:51:54 -0300

    Add link da demo para URL_BASE

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from "../components/Questionario";
 import QuestaoModel from "../models/questao";
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://quiz-leomdrs.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()

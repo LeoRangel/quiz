b055fd6607abc25e136e7912b3bcd9e5bff07cbf
LeoMdrs <leandro.mdrs06@gmail.com>
2021-09-26 19:31:17 -0300

    Ajustar README e aumentar tempo de resposta das perguntas para 10 segundos

--- components/Questionario.tsx
+++ components/Questionario.tsx
@@ -25,7 +25,7 @@ export default function Questionario(props: QuestionarioProps) {
             {props.questao ?
                 <Questao
                     valor={props.questao}
-                    tempoResposta={6}
+                    tempoResposta={10}
                     onResponse={onResponse}
                     tempoEsgotado={props.irParaProximoPasso}
                 />
